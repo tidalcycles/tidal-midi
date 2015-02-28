61d1194024fb5bb64464f8c82e46f67672ce2c26
Lennart Melzer <lennart@melzer.it>
2015-02-28 20:14:58 +0100

    [add] thread-safety

--- Sound/Tidal/Tetra.hs
+++ Sound/Tidal/Tetra.hs
@@ -1,29 +1,23 @@
-
 module Sound.Tidal.Tetra where
 
 import qualified Sound.PortMidi as PM
-import GHC.Word
-import GHC.Int
 
 import Sound.OSC.FD
-import qualified Data.Map as Map
-import Control.Applicative
+
+import Control.Monad
 import Control.Concurrent.MVar
---import Visual
-import Data.Hashable
+
 import Data.Bits
 import Data.Maybe
-import System.Process
+
 import Control.Concurrent
 import Foreign.C
+
 import Sound.Tidal.Stream
 import Sound.Tidal.Pattern
 import Sound.Tidal.Parse
 
-import System.Environment
 import System.IO.Error
-
-import Control.Exception
 
 keys :: OscShape
 keys = OscShape {path = "/note",
@@ -126,45 +120,68 @@ paramRanges = [
 
 keynames = map name (tail $ tail $ params keys)
 
-test :: IO (Maybe a) -> IO a
-test = (>>= maybe (ioError $ userError "oops") return)
+data Output = Output {
+                       conn :: PM.PMStream,
+                       lock :: MVar ()
+                     }
 
-keyproxy latency deviceID = do
+outputDevice :: PM.DeviceID -> IO (Either Output PM.PMError)
+outputDevice deviceID = do
   PM.initialize
-  do
-    result <- PM.openOutput deviceID 0
-    case result of
-      Right err -> putStrLn ("Failed opening Midi Output Port: " ++ show err)
-      Left conn ->
-        do
-          x <- udpServer "127.0.0.1" 7303
-          forkIO $ loop conn x
-          return ()
-            where loop conn x = do m <- recvMessage x
-                                   act conn m
-                                   loop conn x
-                  act conn (Just (Message "/note" (note:dur:ctrls))) =
-                      do
-                        let note' = (fromJust $ d_get note) :: Int
-                            dur' = (fromJust $ d_get dur) :: Float
-                            ctrls' = (map (fromJust . d_get) ctrls) :: [Float]
-                        -- putStrLn ("Message received")
-                        sendmidi latency conn (fromIntegral note', dur') ctrls'
-                        return()
+  result <- PM.openOutput deviceID 0
+  case result of
+    Left dev ->
+      do
+        sem <- newEmptyMVar
+        putMVar sem () -- initially fill MVar to be taken by the first user of this output
+        return (Left Output { conn=dev, lock=sem })
+    Right err -> return (Right err)
 
-sendmidi latency stream (note,dur) ctrls =
+messageLoop oStream oLatency oChannel iPort = do
+  putStrLn ("Starting message loop on port " ++ show iPort ++ " for MIDI channel " ++ show oChannel)
+  x <- udpServer "127.0.0.1" iPort
+  forkIO $ loop oStream x oChannel
+    where loop oStream x oChannel = do m <- recvMessage x
+                                       act oStream m oChannel
+                                       loop oStream x oChannel
+          act oStream (Just (Message "/note" (note:dur:ctrls))) oChannel =
+              do
+                let note' = (fromJust $ d_get note) :: Int
+                    dur' = (fromJust $ d_get dur) :: Float
+                    ctrls' = (map (fromJust . d_get) ctrls) :: [Float]
+                -- putStrLn ("Message received")
+                sendmidi oLatency oStream oChannel (fromIntegral note', dur') ctrls'
+                return()
+
+
+makeStream keys port = stream "127.0.0.1" port keys
+
+keyproxy latency deviceID channels = do
+  let ports = (map (+ 7303) channels)
+      keyStreams = map (makeStream keys) ports
+  econn <- outputDevice deviceID
+
+  case econn of
+    Right err -> error ("Failed opening MIDI Output on Device ID: " ++ show deviceID ++ " - " ++ show err)
+    Left conn ->
+      do
+        zipWithM_ (messageLoop conn latency) channels ports
+        return keyStreams
+
+
+sendmidi latency stream channel (note,dur) ctrls =
   do forkIO $ do threadDelay latency
-                 noteOn stream note 60
+                 noteOn stream channel note 60
                  --  putStrLn ("Send On Msg: " ++ show result)
                  threadDelay (floor $ 1000000 * dur)
-                 noteOff stream note
+                 noteOff stream channel note
                  --  putStrLn ("Send Off Msg: " ++ show result)
                  return ()
      let ctrls' = map floor (zipWith (*) paramRanges ctrls)
          ctrls'' = filter ((>=0) . snd) (zip keynames ctrls')
          --ctrls''' = map (\x -> (x, fromJust $ lookup x ctrls'')) usectrls
      --putStrLn $ show ctrls''
-     sequence_ $ map (\(name, ctrl) -> makeCtrl stream (ctrlN name ctrl)) ctrls''
+     sequence_ $ map (\(name, ctrl) -> makeCtrl stream channel (ctrlN name ctrl)) ctrls''
      return ()
 
 
@@ -199,29 +216,48 @@ ctrlN s _             = error $ "no match for " ++ s
 
 
 
-noteOn :: PM.PMStream -> CLong -> CLong -> IO PM.PMError
-noteOn conn val vel = PM.writeShort conn evt
-    where msg = PM.PMMsg 0x90 val vel
-          evt = PM.PMEvent msg 0
+-- PortMIDI Wrapper
+makeEvent :: (Integral a, Integral b, Bits b) => b -> a -> b -> a -> a -> PM.PMEvent
+makeEvent st n ch v t = PM.PMEvent msg (fromIntegral t)
+  where msg = PM.PMMsg (fromIntegral $ encodeChannel ch st) (fromIntegral n) (fromIntegral v)
 
-noteOff :: PM.PMStream -> CLong -> IO PM.PMError
-noteOff conn val = PM.writeShort conn evt
-    where msg = PM.PMMsg 0x80 val 60
-          evt = PM.PMEvent msg 0
+-- now with a semaphore since PortMIDI is NOT thread safe
+sendEvent :: Output -> PM.PMEvent -> IO (Maybe IOError)
+sendEvent o evt = do
+  let sem = lock o
+  takeMVar sem
+  err <- PM.writeShort (conn o) evt
+  putMVar sem ()
+  case err of
+    PM.NoError -> return Nothing
+    e -> return $ Just (userError ("Error '" ++ show e ++ "' sending Event: " ++ show evt))
 
+-- MIDI Utils
+encodeChannel :: (Bits a, Integral a, Integral b) => a -> a -> b
+encodeChannel ch cc = fromIntegral (((-) ch 1) .|. cc)
 
+-- MIDI Messages
+noteOn :: (Bits a, Integral a, Integral b) => Output -> a -> b -> b -> IO (Maybe IOError)
+noteOn o ch val vel = do
+  let evt = makeEvent 0x90 val ch vel 0
+  sendEvent o evt
 
-makeCtrl :: PM.PMStream -> (CLong, CLong) -> IO PM.PMError
-makeCtrl conn (c, n) = do
-  PM.writeShort conn evtMSBCC
-  PM.writeShort conn evtLSBCC
-  PM.writeShort conn evtMSBVal
-  PM.writeShort conn evtLSBVal
-    where msgMSBCC = PM.PMMsg 0xB0 0x63 (shift (c .&. 0x3F80) (-7))
-          evtMSBCC = PM.PMEvent msgMSBCC 0
-          msgLSBCC = PM.PMMsg 0xB0 0x62 (c .&. 0x7F)
-          evtLSBCC = PM.PMEvent msgLSBCC 0
-          msgMSBVal = PM.PMMsg 0xB0 0x06 (shift (n .&. 0x3F80) (-7))
-          evtMSBVal = PM.PMEvent msgMSBVal 0
-          msgLSBVal = PM.PMMsg 0xB0 0x26 (n .&. 0x7F)
-          evtLSBVal = PM.PMEvent msgLSBVal 0
+noteOff :: (Bits a, Integral a, Integral b) => Output -> a -> b -> IO (Maybe IOError)
+noteOff o ch val = do
+  let evt = makeEvent 0x80 val ch 60 0
+  sendEvent o evt
+
+-- This is sending NRPN
+makeCtrl :: (Bits a, Integral a) => Output -> a -> (CLong, CLong) -> IO (Maybe IOError)
+makeCtrl o ch (c, n) = do
+  let nrpn = makeEvent 0xB0
+      evts = [nrpn 0x63 ch (shift (c .&. 0x3F80) (-7)) 0,
+              nrpn 0x62 ch (c .&. 0x7F)  0,
+              nrpn 0x06 ch (shift (n .&. 0x3F80) (-7)) 0,
+              nrpn 0x26 ch (n .&. 0x7F) 0
+             ]
+  maybeErrors <- mapM (sendEvent o) evts
+  let errors = catMaybes maybeErrors
+  case (length errors) of
+    0 -> return Nothing
+    x -> return $ Just $ userError (show x ++ " errors occurred while sending NRPN Event" ++ show evts)
