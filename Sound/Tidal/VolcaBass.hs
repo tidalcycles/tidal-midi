7e8a54183ee7e769f49000cd229c98c1aa1a623f
Lennart Melzer <lennart@melzer.it>
2015-03-01 17:25:44 +0100

    [fix] simplified volca's

--- Sound/Tidal/VolcaBass.hs
+++ Sound/Tidal/VolcaBass.hs
@@ -1,141 +1,41 @@
-  
 module Sound.Tidal.VolcaBass where
 
-import qualified Sound.ALSA.Sequencer.Address as Addr
-import qualified Sound.ALSA.Sequencer.Client as Client
-import qualified Sound.ALSA.Sequencer.Port as Port
-import qualified Sound.ALSA.Sequencer.Event as Event
-import qualified Sound.ALSA.Sequencer as SndSeq
-import qualified Sound.ALSA.Exception as AlsaExc
-import qualified Sound.ALSA.Sequencer.Connect as Connect
-import GHC.Word
-import GHC.Int
+import Sound.Tidal.Stream (makeI, makeF)
 
-import Sound.OSC.FD
-import qualified Data.Map as Map
-import Control.Applicative
-import Control.Concurrent.MVar
-import Data.Hashable
-import Data.Bits
-import Data.Maybe
-import System.Process
-import Control.Concurrent
+import Sound.Tidal.MIDI.Control
 
-import Sound.Tidal.Stream
-import Sound.Tidal.Pattern
-import Sound.Tidal.Parse
-
-channel = Event.Channel 0
-
-bass :: OscShape
-bass = OscShape {path = "/note",
-                 params = [ I "note" Nothing,
-                            F "dur" (Just (0.05)),
-                            F "slide" (Just (-1)),
-                            F "expression" (Just (-1)),
-                            F "octave" (Just (-1)),
-                            F "lforate" (Just (-1)),
-                            F "lfoint" (Just (-1)),
-                            F "pitch1" (Just (-1)),
-                            F "pitch2" (Just (-1)),
-                            F "pitch3" (Just (-1)),
-                            F "attack" (Just (-1)),
-                            F "decay" (Just (-1)),
-                            F "cutoff" (Just (-1)),
-                            F "gate" (Just (-1))
+bass :: ControllerShape
+bass = ControllerShape { params = [
+                            CC "slide" 5,
+                            CC "expression" 11,
+                            CC "octave" 40,
+                            CC "lforate" 41,
+                            CC "lfoint" 42,
+                            CC "pitch1" 43,
+                            CC "pitch2" 44,
+                            CC "pitch3" 45,
+                            CC "attack" 46,
+                            CC "decay" 47,
+                            CC "cutoff" 48,
+                            CC "gate" 49
                           ],
-                 timestamp = NoStamp,
-                 latency = 0,
-                 namedParams = False,
-                 preamble = []
-                }
+                         duration = ("dur", 0.05),
+                         latency = 0.1
+                       }
 
-bassStream = stream "127.0.0.1" 7303 bass
+oscBass = toOscShape bass
 
-note         = makeI bass "note"
-dur          = makeF bass "dur"
-slide        = makeF bass "slide"
-expression   = makeF bass "expression"
-octave       = makeF bass "octave"
-lforate      = makeF bass "lforate"
-lfoint       = makeF bass "lfoint"
-pitch1       = makeF bass "pitch1"
-pitch2       = makeF bass "pitch2"
-pitch3       = makeF bass "pitch3"
-attack       = makeF bass "attack"
-decay        = makeF bass "decay"
-cutoff       = makeF bass "cutoff"
-gate         = makeF bass "gate"
-
-bassnames = map name (tail $ tail $ params bass)
-
-bassproxy latency midiport = 
-   do h <- SndSeq.openDefault SndSeq.Block
-      Client.setName (h :: SndSeq.T SndSeq.OutputMode) "Tidal"
-      c <- Client.getId h
-      p <- Port.createSimple h "out"
-           (Port.caps [Port.capRead, Port.capSubsRead]) Port.typeMidiGeneric
-      conn <- Connect.createTo h p =<< Addr.parse h midiport
-      x <- udpServer "127.0.0.1" 7303
-      forkIO $ loop h conn x
-      return ()
-         where loop h conn x = do m <- recvMessage x
-                                  act h conn m
-                                  loop h conn x
-               act h conn (Just (Message "/note" (note:dur:ctrls))) = 
-                   do -- print $ "Got note " ++ show note
-                      let note' = (fromJust $ d_get note) :: Int
-                          dur' = (fromJust $ d_get dur) :: Float
-                          ctrls' = (map (fromJust . d_get) ctrls) :: [Float]
-                      sendmidi latency h conn (fromIntegral note', dur') ctrls'
-                      return ()
-
-sendmidi latency h conn (note,dur) ctrls = 
-  do forkIO $ do threadDelay latency
-                 Event.outputDirect h $ noteOn conn note 60
-                 threadDelay (floor $ 1000000 * dur)
-                 Event.outputDirect h $ noteOff conn note
-                 return ()
-     let ctrls' = map (floor . (* 127)) ctrls
-         ctrls'' = filter ((>=0) . snd) (zip bassnames ctrls')
-     sequence_ $ map (\(name, ctrl) -> Event.outputDirect h $ makeCtrl conn (ctrlN name ctrl)) ctrls''
-     return ()
-
-ctrlN "slide" v         = (5, v)
-ctrlN "expression" v    = (11, v)
-ctrlN "octave" v        = (40, v)
-ctrlN "lforate" v       = (41, v)
-ctrlN "lfoint" v        = (42, v)
-ctrlN "pitch1" v        = (43, v)
-ctrlN "pitch2" v        = (44, v)
-ctrlN "pitch3" v        = (45, v)
-ctrlN "attack" v        = (46, v)
-ctrlN "decay" v         = (47, v)
-ctrlN "cutoff" v        = (48, v)
-ctrlN "gate" v          = (49, v)
-ctrlN s _               = error $ "no match for " ++ s
-
-noteOn :: Connect.T -> Word8 -> Word8 -> Event.T
-noteOn conn val vel = 
-  Event.forConnection conn 
-  $ Event.NoteEv Event.NoteOn
-  $ Event.simpleNote channel
-                     (Event.Pitch (val))
-                     (Event.Velocity vel)
-
-noteOff :: Connect.T -> Word8 -> Event.T
-noteOff conn val = 
-  Event.forConnection conn 
-  $ Event.NoteEv Event.NoteOff
-  $ Event.simpleNote channel
-                     (Event.Pitch (val))
-                     (Event.normalVelocity)
-
-makeCtrl :: Connect.T -> (Word32, GHC.Int.Int32) -> Event.T
-makeCtrl conn (c, n) = 
-  Event.forConnection conn 
-  $ Event.CtrlEv Event.Controller $ Event.Ctrl 
-                                    channel 
-                                    (Event.Parameter c) 
-                                    (Event.Value n)
-
+note         = makeI oscBass "note"
+dur          = makeF oscBass "dur"
+slide        = makeF oscBass "slide"
+expression   = makeF oscBass "expression"
+octave       = makeF oscBass "octave"
+lforate      = makeF oscBass "lforate"
+lfoint       = makeF oscBass "lfoint"
+pitch1       = makeF oscBass "pitch1"
+pitch2       = makeF oscBass "pitch2"
+pitch3       = makeF oscBass "pitch3"
+attack       = makeF oscBass "attack"
+decay        = makeF oscBass "decay"
+cutoff       = makeF oscBass "cutoff"
+gate         = makeF oscBass "gate"
