e814e2b49c3004927f19b82079cc9f69f920b289
Lennart Melzer <lennart@melzer.it>
2017-03-10 07:57:25 +0100

    remove stray simplesynth module, normalize synth module

--- Sound/Tidal/MIDI/Synth.hs
+++ Sound/Tidal/MIDI/Synth.hs
@@ -1,17 +1,18 @@
 module Sound.Tidal.MIDI.Synth where
 
-import Sound.Tidal.Params
 import Sound.Tidal.MIDI.Control
+import Sound.Tidal.Params
 
 synthController :: ControllerShape
-synthController = ControllerShape {
-  controls = [
-    mCC modwheel_p 1,
-    mCC pan_p 10,
-    mCC expression_p 11,
-    mCC sustainpedal_p 64
-     ],
-  latency = 0.04
+synthController =
+  ControllerShape
+  { controls =
+      [ mCC modwheel_p 1
+      , mCC pan_p 10
+      , mCC expression_p 11
+      , mCC sustainpedal_p 64
+      ]
+  , latency = 0.1
   }
 
 synth = toShape synthController
